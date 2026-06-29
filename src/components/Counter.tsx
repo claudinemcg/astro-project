b563aa59d13ba5816e9751d56ca177f822317ead
claudinemcg <claudine.mcgowan@gmail.com>
2026-06-29 18:52:23 +0100

    client load

---
+++ src/components/Counter.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <p>{count}</p>
+            <button type="button" onClick={() => setCount(count - 1)}>-</button>
+            <button type="button" onClick={() => setCount(count + 1)}>+</button>
+        </div>
+    );
+}
